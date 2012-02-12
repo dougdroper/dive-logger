d856f28f859571ac8b4c27bcdca20129c3827d36
Douglas Roper <douglasroper@notonthehighstreet.com>
2012-02-12 20:17:09 +0000

    rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiveLogger3::Application.config.secret_token = '163ce16141eaa61c2da0445e8bc17e32f29a2a1f299f57fa4e170596a3784cdba6a562451173eab0fc345c9bf34f0eccf3b1c7ddf2b868dc1e734f105923d21b'
