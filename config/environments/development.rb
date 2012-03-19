24ed31b88fdab3d11fc3c9ce5146d50a27b462d0
Douglas Roper <douglasroper@notonthehighstreet.com>
2012-03-19 21:12:39 +0000

    rails stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,5 @@ DiveLogger3::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
